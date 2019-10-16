0f4c0b888886c527730eb5aa4e8fe380eea4acd1
Claire Pickhardt <cpickhardt@itx.com>
2019-10-16 10:09:47 -0400

    table class

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,9 +38,26 @@ $(function () {
 });
 
 // Add pagination to data table containing cards
-$(document).ready(function () {
-    $('#section02').DataTable({
-        "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
+$(document).ready(function(){
+    $('#section02').after('<div id="nav"></div>');
+    var rowsShown = 4;
+    var rowsTotal = $('#section02 tbody tr').length;
+    var numPages = rowsTotal/rowsShown;
+    for(i = 0;i < numPages;i++) {
+        var pageNum = i + 1;
+        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+    }
+    $('#section02 tbody tr').hide();
+    $('#section02 tbody tr').slice(0, rowsShown).show();
+    $('#nav a:first').addClass('active');
+    $('#nav a').bind('click', function(){
+
+        $('#nav a').removeClass('active');
+        $(this).addClass('active');
+        var currPage = $(this).attr('rel');
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $('#section02 tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+                css('display','table-row').animate({opacity:1}, 300);
     });
-    $('.dataTables_length').addClass('bs-select');
-  });
+});
